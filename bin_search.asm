c2194c5d04156b3fb5611674b094f11a5125bcc1
Rishabh Verma <10rishabhv@gmail.com>
2023-02-17 04:42:01 +0530

    improve part2 comments

--- bin_search.asm
+++ bin_search.asm
@@ -20,7 +20,7 @@ main:
     add $a0, $t1, $zero # allocate (4*n) bytes in heap
     li $v0, 9
     syscall
-    add $s4, $v0, $zero
+    add $s4, $v0, $zero # s1 = base address of allocated heap memory
     add $t1, $v0, $zero
 
 input_loop:
@@ -35,7 +35,7 @@ input_loop:
 input_x:
     li $v0, 5
     syscall
-    sw $v0, x # store the input integer at the address of x
+    sw $v0, x # store input integer at the address of x
     lw $s1, x # s1 = x
     lw $s3, n # s3 = n (right bound)
     li $s5, 0 # s5 = 0 (left bound)
@@ -61,16 +61,16 @@ input_x:
 # initially l = 0 and r = n
 binary_search:
     add $s0, $s5, $s3 # s0 = l+r
-    srl $s0, $s0, 1 # s0 = floor((l+r)/2)
-    sll $s0, $s0, 2 # s0 = 4*floor((l+r)/2)
-    add $t5, $s4, $s0 # t5 = base address of array + 4*floor((l+r)/2)
-    lw $t6, 0($t5) # t6 = a[floor((l+r)/2)]
+    srl $s0, $s0, 1 # s0 = i = floor((l+r)/2)
+    sll $s0, $s0, 2 # s0 = 4*i
+    add $t5, $s4, $s0 # t5 = base address of array + 4*i
+    lw $t6, 0($t5) # t6 = a[i]
     beq $t6, $s1, found
     blt $t6, $s1, shift_left_bound # if a[i] < x, update l
     j shift_right_bound # else update r
 
 shift_left_bound:
-    srl $s0, $s0, 2 # s0 = floor((l+r)/2)
+    srl $s0, $s0, 2 # s0 = i = floor((l+r)/2)
     addi $s5, $s0, 1 # l = i+1
     blt $s5, $s3, binary_search # loop if l < r
     j not_found
