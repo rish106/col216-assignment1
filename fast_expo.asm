adc16a6d4fddf6c6a0209f19f3c20cfb48096d8b
Rishabh Verma <10rishabhv@gmail.com>
2023-02-18 23:37:24 +0530

    improve part3 formatting

--- fast_expo.asm
+++ fast_expo.asm
@@ -3,69 +3,64 @@
 .text
 main: 
 
-    li $v0,5
+    li $v0, 5
     syscall
-    add $a2,$v0,$zero  #Taking base input
+    add $a2, $v0, $zero  # Taking base input
 
-    li $v0,5
+    li $v0, 5
     syscall
-    add $a3,$v0,$zero  #Taking exponent input
+    add $a3, $v0, $zero  # Taking exponent input
 
-    addi $sp,$sp,-4
-    sw $ra,0($sp)
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
     jal binexp
-    lw $ra,0($sp)
-    addi $sp,$sp,4
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
 
-    li $v0,1
-    add $a0,$v1,$zero
+    li $v0, 1
+    add $a0, $v1, $zero
     syscall
 
     jr $ra
 
 
-#Starting the function
+# Starting the function
 
 binexp:
 
-#a2 and a3 would always be storing the base and exponent respectively during
-#each function call
+# a2 and a3 would always be storing the base and exponent respectively during
+# each function call
 
-    beq $a3,$zero,Basecase     #the base case of the recursion is when the exponent is 0
+    beq $a3, $zero, Basecase     # the base case of the recursion is when the exponent is 0
 
-    addi $sp,$sp,-8
-    sw $ra,0($sp)
-    sw $a3,4($sp)   #Store the current values of ra and a3 on the stack
+    addi $sp, $sp, -8
+    sw $ra, 0($sp)
+    sw $a3, 4($sp)   # Store the current values of ra and a3 on the stack
 
-    srl $a3,$a3,1  #change the value of exponent to floor(exponent/2)
+    srl $a3, $a3, 1  # change the value of exponent to floor(exponent/2)
     jal binexp
 
-    lw $a3,4($sp)
-    lw $ra,0($sp)
-    addi $sp,$sp,8 #Restore the saved values in the respective registers
+    lw $a3, 4($sp)
+    lw $ra, 0($sp)
+    addi $sp, $sp, 8 # Restore the saved values in the respective registers
 
-    andi $t0,$a3,1
-    li $t1,1    #To check the parity of a3, we check its bitwise AND with 1
-    beq $t0,$t1,Oddcase     #If AND is 1, the exponent is odd
-    beq $t0,$zero,Evencase  #Else it is even
+    andi $t0, $a3, 1
+    li $t1, 1    # To check the parity of a3, we check its bitwise AND with 1
+    beq $t0, $t1, Oddcase     # If AND is 1, the exponent is odd
+    beq $t0, $zero, Evencase  # Else it is even
    
 Basecase:
 
-    li $v1,1
+    li $v1, 1
     jr $ra
 
 Oddcase:
 
-    mul $v1,$v1,$v1
-    mul $v1,$v1,$a2   #We know a2^((a3-1)/2) and we are finding $a2^a3$ from that
+    mul $v1, $v1, $v1
+    mul $v1, $v1, $a2   # We know a2^((a3-1)/2) and we are finding $a2^a3$ from that
     jr $ra
 
 Evencase:
 
-    mul $v1,$v1,$v1  #We know a2^(a3/2) and we are finding $a2^a3$ from that
+    mul $v1, $v1, $v1  # We know a2^(a3/2) and we are finding $a2^a3$ from that
     jr $ra
-
-
-    
-
-
